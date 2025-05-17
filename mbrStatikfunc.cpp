83729ec321fc2969e67fca559ae5fa50fa4b99a0
Daffansyah276 <daffansyaharya@gmail.com>
2025-05-17 17:11:05 +0700

    membuat class public dalam mbrStatikFunc

--- mbrStatikfunc.cpp
+++ mbrStatikfunc.cpp
@@ -7,4 +7,13 @@ private:
     static int nim;
     int id;
     string nama;
+
+public:
+    void setID();
+    void printAll();
+
+    static void setNim(int pNim) { nim = pNim; } /*Definisi Function*/
+    static int getNim() { return nim; }         /*Definisi Function*/
+
+    mahasiswa(string pNama) : nama(pNama) { setID(); }
 };
