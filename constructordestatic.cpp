c81706904356ba27a88ec66f941b5f816f258a3d
Daffansyah276 <daffansyaharya@gmail.com>
2025-05-19 15:40:06 +0700

    ConstructorDestatic

---
+++ constructordestatic.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa {
+    private:
+        static int jmlmhs;
+    public:
+        string status;
+        string nama;
+        int nim;
+        mahasiswa(string pnama, int pnim){
+            status = " Mahasiswa Baru " ; 
+            nama = pnama;
+            nim = pnim;
+            ++ jmlmhs ;
+            cout << " mahasiswa dibuat " << endl;
+            cout << " Status = " << status << endl;
+        };
+        ~mahasiswa(){
+            cout << " Mahasiswa dengan nama " << nama << "Dihancurkan " <<  endl;
+            --jmlmhs;
+        };
+        static void gettotalmhs(){
+            cout << " Jumlah mahasiswa = " << jmlmhs << endl;
+
+        };
+};
+
+int mahasiswa :: jmlmhs = 0;
+
+int main(){
+    cout << " Jumlah Mahasiswa = " << mahasiswa::gettotalmhs() << endl;
+    mahasiswa mhs1("Rudy",28);
+    mahasiswa mhs2("Joko",73);
+    mhs2.status = " Mahasiswa uzur";
+    cout << " mhs2.status = " << endl;
+    mahasiswa mhs3("Wobo",69);
+    cout << " Jumlah Mahasiswa = " << mahasiswa::gettotalmhs() << endl;
+    {
+        mahasiswa mhs4("pra",98);
+        cout << " Jumlah Mahasiswa dalam bracket = " << mahasiswa::gettotalmhs() << endl;
+    }
+    cout << " Jumlah Mahasiswa = " << mahasiswa::gettotalmhs() << endl;
+    return 0;
+
+}
